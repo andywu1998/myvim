4e4043b3fdfa621b175f8416f40c7ea75cc8cc71
wuliwei <wuliwei@chuangzuomao.cn>
2023-08-19 15:14:46 +0800

    install and setup toggleterm

--- lua/plugins/toggleterm-config.lua
+++ lua/plugins/toggleterm-config.lua
@@ -1,14 +1,8 @@
 return {
-  {
-    "akinsho/toggleterm.nvim",
-    tag = "*",
-    keys = {
-      {
-        "<leader>td",
-        "<cmd>ToggleTerm size=40 dir=~/Desktop direction=horizontal<cr>",
-        desc = "Open a horizontal terminal at the Desktop directory"
-      }
+    -- Added this plugin.
+    {
+        'akinsho/toggleterm.nvim',
+        tag = "*",
+        config = true
     },
-  },
-
 }
