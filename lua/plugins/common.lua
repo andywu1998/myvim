f24dfca93be0bf28b118f6bd5d19352eeceb3b8b
wuliwei <wuliwei@chuangzuomao.cn>
2023-08-20 11:30:29 +0800

    停用example.lua，自己检查插件列表

---
+++ lua/plugins/common.lua
@@ -0,0 +1,50 @@
+return {
+    -- add symbols-outline
+    -- 显示outline
+    {
+        "simrat39/symbols-outline.nvim",
+        cmd = "SymbolsOutline",
+        keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+        config = true,
+    },
+    {
+        -- 查找文件
+        "nvim-telescope/telescope.nvim",
+        keys = {
+            -- add a keymap to browse plugin files
+            -- stylua: ignore
+            {
+                "<leader>fp",
+                function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+                desc = "Find Plugin File",
+            },
+        },
+        -- change some options
+        opts = {
+            defaults = {
+                layout_strategy = "horizontal",
+                layout_config = { prompt_position = "top" },
+                sorting_strategy = "ascending",
+                winblend = 0,
+            },
+        },
+    },
+    {
+        "telescope.nvim",
+        dependencies = {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+            config = function()
+                require("telescope").load_extension("fzf")
+            end,
+        },
+    },
+    -- 选定主题
+    {
+
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "gruvbox",
+        },
+    },
+}
