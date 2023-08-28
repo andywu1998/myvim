3656b1c79727967ff8b6db660a8df25f2bdb65e4
andywu1998 <1078539713@qq.com>
2023-08-28 15:47:20 +0800

    tcd打开当前目录

--- lua/config/toggleterm-config.lua
+++ lua/config/toggleterm-config.lua
@@ -105,3 +105,17 @@ function _CWD_TOGGLE()
 end
 
 vim.api.nvim_set_keymap("n", "<leader>tcw", "<cmd>lua _CWD_TOGGLE()<CR>", { noremap = true, silent = true })
+
+function _CWD_TERM()
+    local current_file_directory = vim.fn.expand('%:p:h')
+    local term = Terminal:new({
+        cmd = "zsh",
+        dir = current_file_directory,
+        hidden = false,
+        close_on_exit = false,
+    })
+    vim.notify(cmd)
+    term:open()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>tcd", "<cmd>lua _CWD_TERM()<CR>", { noremap = true, silent = true })
