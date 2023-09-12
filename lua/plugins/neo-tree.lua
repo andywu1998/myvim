416e210c93bd4fddb16c5487a0755de941758b61
andywu1998 <1078539713@qq.com>
2023-09-12 11:07:24 +0800

    feat(tmux)：添加tmux.conf

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,26 +1,45 @@
 return {
-  { "nvim-tree/nvim-web-devicons", lazy = true },
-  { "MunifTanjim/nui.nvim", lazy = true },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      -- char = "▏",
-      char = "│",
-      filetype_exclude = {
-        "help",
-        "alpha",
-        "dashboard",
-        "neo-tree",
-        "Trouble",
-        "lazy",
-        "mason",
-        "notify",
-        "toggleterm",
-        "lazyterm",
-      },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
+    { "nvim-tree/nvim-web-devicons", lazy = true },
+    { "MunifTanjim/nui.nvim", lazy = true },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = {
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    show_hidden_count = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = true,
+                    hide_by_name = {
+                        -- '.git',
+                        -- '.DS_Store',
+                        -- 'thumbs.db',
+                    },
+                    never_show = {},
+                },
+            },
+        },
     },
-  },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = { "BufReadPost", "BufNewFile" },
+        opts = {
+            -- char = "▏",
+            char = "│",
+            filetype_exclude = {
+                "help",
+                "alpha",
+                "dashboard",
+                "neo-tree",
+                "Trouble",
+                "lazy",
+                "mason",
+                "notify",
+                "toggleterm",
+                "lazyterm",
+            },
+            show_trailing_blankline_indent = false,
+            show_current_context = false,
+        },
+    },
 }
