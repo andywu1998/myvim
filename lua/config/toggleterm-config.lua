ffd90a0a591c0b044f566fdb8354e5c266ff15dd
liweiwu.wlw <liweiwu.wlw@xiaoman.cn>
2024-05-29 16:25:49 +0800

    feat(暂时禁用powershell 之后在win上再打开)

--- lua/config/toggleterm-config.lua
+++ lua/config/toggleterm-config.lua
@@ -1,18 +1,18 @@
 -- toggleterm-config.lua
 
 
-local powershell_options = {
-  shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-  shellquote = "",
-  shellxquote = "",
-}
-
-for option, value in pairs(powershell_options) do
-  vim.opt[option] = value
-end
+-- local powershell_options = {
+--   shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+--   shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+--   shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+--   shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+--   shellquote = "",
+--   shellxquote = "",
+-- }
+--
+-- for option, value in pairs(powershell_options) do
+--   vim.opt[option] = value
+-- end
 
 local status_ok, toggleterm = pcall(require, "toggleterm")
 if not status_ok then
@@ -40,7 +40,6 @@ toggleterm.setup({
             background = "Normal",
         },
     },
-    default_executive = 'powershell.exe'
 })
 
 function _G.set_terminal_keymaps()
