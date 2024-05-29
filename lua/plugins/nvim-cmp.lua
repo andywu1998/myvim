9e63674d5ba9506f4b3b5ee5ab47cd1313bb0fc3
liweiwu.wlw <liweiwu.wlw@xiaoman.cn>
2024-05-29 15:44:46 +0800

    feat(disable nvim-cmp-lsp when open php)

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -6,5 +6,14 @@ return {
             return false
         end
     end,
+    },
+    {
+        "hrsh7th/nvim-cmp-lsp",
+        enabled = function()
+        if vim.bo.filetype == "php" then
+            return false 
+        end
+    end,
     }
+
 }
