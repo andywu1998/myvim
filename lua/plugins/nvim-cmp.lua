1e542c9318c218fe04275a5d4712c6dad56403b2
liweiwu.wlw <liweiwu.wlw@xiaoman.cn>
2024-05-30 09:50:50 +0800

    注释掉php的特殊处理（可能后面会有php出现两个代码补全的问题）

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,19 +1,19 @@
 return {
     {
         "hrsh7th/nvim-cmp",
-        enabled = function()
-        if vim.bo.filetype == "php" then
-            return false
-        end
-    end,
+        -- enabled = function()
+        --     if vim.bo.filetype == "php" then
+        --         return false
+        --     end
+        -- end,
     },
     {
         "hrsh7th/nvim-cmp-lsp",
-        enabled = function()
-        if vim.bo.filetype == "php" then
-            return false 
-        end
-    end,
+        -- enabled = function()
+        --     if vim.bo.filetype == "php" then
+        --         return false 
+        --     end
+        -- end,
     }
 
 }
