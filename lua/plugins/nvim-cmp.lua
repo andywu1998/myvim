0a56beb225f30baec4392fae740e54926cd580c2
liweiwu.wlw <liweiwu.wlw@xiaoman.cn>
2024-05-30 11:01:58 +0800

    feat 不disable nvim-cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -7,13 +7,13 @@ return {
         --     end
         -- end,
     },
-    {
-        "hrsh7th/nvim-cmp-lsp",
-        -- enabled = function()
-        --     if vim.bo.filetype == "php" then
-        --         return false 
-        --     end
-        -- end,
-    }
+    -- {
+    --     "hrsh7th/nvim-cmp-lsp",
+    --     -- enabled = function()
+    --     --     if vim.bo.filetype == "php" then
+    --     --         return false 
+    --     --     end
+    --     -- end,
+    -- }
 
 }
